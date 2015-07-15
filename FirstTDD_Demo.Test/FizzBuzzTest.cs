418e5338e4ee506e2e27b2ec5fc7e89a5ef90af3
aKratzke <arosekratzke@gmail.com>
2015-07-15 16:12:32 -0400

    2015.07.15
    After one successful test and one unsuccessful test

---
+++ FirstTDD_Demo.Test/FizzBuzzTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FirstTDD_Demo.Test
+{
+    public class FizzBuzzTest   
+    {
+        [Fact]
+        public void ReturnedNumberAsString()
+        {
+            var ourTest = new FirstTDD_Demo.FizzBuzzLogic();
+
+            string answer = ourTest.DoTheThing(1);
+
+            Assert.Equal("1", answer);
+        }
+        [Fact]
+        public void NumberDividedByThree()
+        {
+            var ourTest = new FirstTDD_Demo.FizzBuzzLogic();
+            string answer = ourTest.DoTheThing(3);
+
+            Assert.Equal("Fizz", answer);
+        }
+    }
+}
---
+++ FirstTDD_Demo/FizzBuzzLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTDD_Demo
+{
+    public class FizzBuzzLogic
+    {
+        public string DoTheThing(int p)
+        {
+            string answer = "";
+
+            if(p % 3 != 0 && p % 5 != 0)
+            answer = p.ToString();
+            else
+            {
+                answer = (p % 3 == 0) ? "Fizz" : string.Empty;
+            }
+            
+            return answer;
+        }
+    }
+}
